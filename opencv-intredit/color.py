0ad8d6d8ecd3a7f33e8e453fadb328fcc615ee96
Chubak <chubakbidpaa@gmail.com>
2022-04-26 05:28:47 +0430

    fixed: pep8

--- opencv-intredit/color.py
+++ opencv-intredit/color.py
@@ -3,9 +3,9 @@ import numpy as np
 from typing import Tuple, List
 
 
-def enclose_red(img: np.array, 
-        lower_thrershold: Tuple[Tuple[int, int], Tuple[int, int]],
-        upper_thrershold: Tuple[Tuple[int, int], Tuple[int, int]]) -> np.array:
+def enclose_red(img: np.array,
+                lower_thrershold: Tuple[Tuple[int, int], Tuple[int, int]],
+                upper_thrershold: Tuple[Tuple[int, int], Tuple[int, int]]) -> np.array:
     """
     Detects and encloses red colors based on between threshold
 
@@ -14,7 +14,6 @@ def enclose_red(img: np.array,
         threshold: The red threshold tuple
     """
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
 
     lower_mask = cv2.inRange(hsv_img, lower_thrershold[0], lower_thrershold[1])
     upper_mask = cv2.inRange(hsv_img, upper_thrershold[0], upper_thrershold[1])
