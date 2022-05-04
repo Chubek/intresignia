ac8868a29c1f20d7ff12c6fd3de894b5ad314a6c
Chubak <chubakbidpaa@gmail.com>
2022-05-04 17:37:27 +0430

    changed: color op; added: auto brightness

--- src/intresignia/color.py
+++ src/intresignia/color.py
@@ -4,12 +4,15 @@ from typing import List
 import cv2
 import numpy as np
 
+from . import auto_brighten
+
 
 def enclose_red(img: np.array,
                 lower_thrershold=((120, 50, 50), (150, 255, 255)),
                 upper_thrershold=((175, 60, 50), (180, 255, 255)),
                 red_thresh=125,
-                op=False) -> np.array:
+                op=False,
+                add_hue=40) -> np.array:
     """
     This function takes four arguments and isolates the red color. The red
     image is later given to HoughCircles to detect circles so it's necessary that image is clean
@@ -33,8 +36,7 @@ def enclose_red(img: np.array,
         saturated spots in the image. Pass a value that you think would represent
         the R, the G and the B of the sign and this single value acts for al l3.
     op: bool
-        The image is put through normalization, brightening, saturating, opening
-        and blurring if this is passed (prefrably on)
+        Do operations on the HSV image or not
 
     Returns
         final_image: np.array
@@ -44,22 +46,31 @@ def enclose_red(img: np.array,
     kernel = cv2.getStructuringElement(
         shape=cv2.MORPH_ELLIPSE, ksize=(9, 9))
 
+    img, _, _ = auto_brighten.automatic_brightness_and_contrast(img)
+
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     copy_img = img.copy()
 
     if op:
-        copy_img[:, :, 0] = copy_img[:, :, 0] + 20
-        copy_img[:, :, 2] = 255
+        hsv_img[:, :, 0] = np.where(
+            (hsv_img[:, :, 0] > 100 - add_hue) &
+            (hsv_img[:, :, 0] < 100 + (add_hue * 2)),
+            hsv_img[:, :, 0] + add_hue, hsv_img[:, :, 0])
+
+        hsv_img[:, :, 0] = np.clip(hsv_img[:, :, 0], 0, 180)
 
         normed = cv2.normalize(hsv_img, None, 0, 180,
                                cv2.NORM_MINMAX, cv2.CV_8UC1)
 
-        opened = cv2.morphologyEx(normed, cv2.MORPH_OPEN, kernel)
-        hsv_img = cv2.GaussianBlur(opened, (5, 5), 0)
+        close = cv2.morphologyEx(normed, cv2.MORPH_CLOSE, kernel)        
 
-    lower_mask = cv2.inRange(hsv_img, lower_thrershold[0], lower_thrershold[1])
-    upper_mask = cv2.inRange(hsv_img, upper_thrershold[0], upper_thrershold[1])
+        hsv_img = cv2.GaussianBlur(normed, (5, 5), 0)
+
+    lower_mask = cv2.inRange(hsv_img, lower_thrershold[0],
+                             lower_thrershold[1])
+    upper_mask = cv2.inRange(hsv_img, upper_thrershold[0],
+                             upper_thrershold[1])
 
     isolated = cv2.bitwise_and(
         copy_img, copy_img, mask=lower_mask + upper_mask)
