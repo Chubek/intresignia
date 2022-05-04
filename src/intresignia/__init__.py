2c683dd6ef086fdce4ddb982446b0a4a336e7167
Chubak <chubakbidpaa@gmail.com>
2022-05-04 22:25:19 +0430

    fixed: __init__

--- src/intresignia/__init__.py
+++ src/intresignia/__init__.py
@@ -4,8 +4,8 @@ import sys
 sys.path.append(os.path.basename(os.path.dirname(__file__)))
 
 
-globals()['Settings'] = __import__("settings.Settings")
-globals()['intresignia_detect'] = __import__("detect.intresignia_detect")
+globals()['Settings'] = __import__(".settings.Settings")
+globals()['intresignia_detect'] = __import__(".detect.intresignia_detect")
 
 
 all = ["settings.Settings", "detect.intresignia_detect"]
