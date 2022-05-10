e576ebaaef08b92cd1830bd3d9d8454782b47f70
Chubak <chubakbidpaa@gmail.com>
2022-05-10 21:45:42 +0430

    fixed: bool to len

--- src/intresignia/detect.py
+++ src/intresignia/detect.py
@@ -293,7 +293,7 @@ def intresignia_detect_alt(img_path: str, stn: st.Settings, pyrd=True, resize=(8
             algo=stn.circle_algo
         )
 
-        if not bool(circles_):
+        if len(circles_) == 0:
             p("No circles detected in the cropped image, continuing...")
             continue
 
